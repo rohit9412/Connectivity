82d631138b227fdc95abfd14dc028d3668315fbe
Rohit Kumar Ranjan <rohit.ranjan@whitelabel-loyalty.com>
2022-03-14 03:17:02 +0530

    Adding support for swift package manager

---
+++ Sources/Connectivity/Connectivity.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  
+//
+//  Created by Rohit Kumar Ranjan on 13/03/22.
+//
+
+#ifndef Reachability_h
+#define Reachability_h
+
+#import "Reachability.h"
+
+#endif /* Reachability_h */
--- Tests/ConnectivityTests/ConnectivityTests.swift
+++ Tests/ConnectivityTests/ConnectivityTests.swift
@@ -6,6 +6,6 @@ final class ConnectivityTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Connectivity().text, "Hello, World!")
+//        XCTAssertEqual(Connectivity().text, "Hello, World!")
     }
 }
